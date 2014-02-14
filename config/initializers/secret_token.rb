63c4cae6c44faa8a32ca0463ce79f9ecf81a7047
Gustavo Duarte <gus.duarte@gmail.com>
2014-02-14 10:38:43 -0200

    Initializin the repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MedicalCalendar::Application.config.secret_key_base = '8e8d0dc1355822e2c0e2effbb68290a463cd4dbec3f4d1d9df9879792b17d851eb018d2d4868580fbc3d97aee4dc3af03a5d46ff6a5001cdd4e66760224341b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MedicalCalendar::Application.config.session_store :cookie_store, key: '_medical_calendar_session'
